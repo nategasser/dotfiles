bba461644c67c4fb77dfb172d195d679099050c8
Nathan Gasser <ngasser@rockriverstar.com>
2024-05-09 15:40:42 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-sudo apt -y install emacs-nox whois rename jq
-# cp ~/rrs/conf/dot-emacs ~/.emacs
-# mkdir -p ~/.emacs.d
-# cp ~/rrs/conf/emacs.d/* ~/.emacs.d
+# tools
+apt -y install emacs-nox whois rename jq
